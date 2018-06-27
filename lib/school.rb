4f11d2655922c4342415a01e63752809a699da69
Joana Palero <joana.palero24@gmail.com>
2018-06-27 02:13:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,10 +4,18 @@ require 'pry'
 class School
   attr_accessor :roster
   attr_reader :name
+  
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-
+  def add_student(name, grade)
+    if
+      @roster[grade] == NIL
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
 end
